6dd3ee080f8cfcfc1b5d57fe33bd2ebdc58b1d09
minhduc <minhductxqt@gmail.com>
2023-05-20 15:44:25 +0700

    feat: uber

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { KeyboardAvoidingView, Platform } from 'react-native'
+import { Provider } from 'react-redux'
+import { store } from './store'
+import HomeScreen from './screens/HomeScreen'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import MapScreen from './screens/MapScreen'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={Platform.OS === 'ios' ? -64 : 0}
+            style={{ flex: 1 }}
+          >
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+              <Stack.Screen name='HomeScreen' component={HomeScreen} />
+              <Stack.Screen name='MapScreen' component={MapScreen} />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </Provider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/NavigateCard.js
@@ -0,0 +1,95 @@
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity
+} from 'react-native'
+import React from 'react'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import { GOOGLE_MAPS_APIKEY } from '@env'
+import { useDispatch } from 'react-redux'
+import { setDestination } from '../slices/navSlice'
+import { useNavigation } from '@react-navigation/native'
+import NavFavourites from './NavFavourites'
+import { Icon } from '@rneui/base'
+
+const NavigateCard = () => {
+  const dispatch = useDispatch()
+  const navigation = useNavigation()
+
+  return (
+    <SafeAreaView className='bg-white flex-1'>
+      <Text className='text-center py-5 text-xl'>Good Morning, Duc</Text>
+      <View className='border-t border-gray-200 flex-shrink'>
+        <View>
+          <GooglePlacesAutocomplete
+            placeholder='Where to?'
+            styles={toInputBoxStyles}
+            nearbyPlacesAPI='GooglePlacesSearch'
+            debounce={400}
+            enablePoweredByContainer={false}
+            fetchDetails={true}
+            query={{
+              key: GOOGLE_MAPS_APIKEY,
+              language: 'en'
+            }}
+            returnKeyType={'search'}
+            minLength={2}
+            onPress={(data, details = null) => {
+              dispatch(
+                setDestination({
+                  location: details.geometry.location,
+                  description: data.description
+                })
+              )
+
+              navigation.navigate('RideOptionsCard')
+            }}
+          />
+        </View>
+
+        <NavFavourites />
+      </View>
+
+      <View className='flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100'>
+        <TouchableOpacity
+          className='flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full'
+          onPress={() => navigation.navigate('RideOptionsCard')}
+        >
+          <Icon name='car' type='font-awesome' color='white' size={16} />
+          <Text className='text-white text-center'>Rides</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity className='flex flex-row justify-between w-24 px-4 py-3 rounded-full'>
+          <Icon
+            name='fast-food-outline'
+            type='ionicon'
+            color='black'
+            size={16}
+          />
+          <Text className='text-center'>Eats</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default NavigateCard
+
+const toInputBoxStyles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    paddingTop: 20,
+    flex: 0
+  },
+  textInput: {
+    backgroundColor: '#DDDDDF',
+    borderRadius: 0,
+    fontSize: 18
+  },
+  textInputContainer: {
+    paddingHorizontal: 20,
+    paddingBottom: 0
+  }
+})
